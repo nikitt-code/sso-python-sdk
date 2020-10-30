08175a64cc3ed39a046460599a1614f6a1b8bb10
Никита Предеин <67558540+nikitt-code@users.noreply.github.com>
2020-10-30 22:41:43 +0500

    Example

---
+++ main.py
@@ -0,0 +1,26 @@
+from ssosdk import SSOSDK
+"""
+SSO PYTHON SDK
+author: vk.com/niki_tt
+Поддержи разработчика: https://vk.com/app7602416#to=463406970
+MIT, All Rights Reserved
+"""
+
+if __name__ == '__main__':
+    app = SSOSDK("YOUR_TOKEN")
+
+    # users.get
+    # users_array - array of ids, if u need one user [ id ]
+    # app.usersGet([ 463406970 ])
+
+    # transfer.create
+    # id - recipient id, count - value of send coins
+    # app.transfersCreate(id, count)
+
+    # transfers.getHistory
+    # count - count of transfers to show
+    # app.transfersGet(count)
+
+    # webhooks.create
+    # url - url to your server
+    # app.webhooksCreate(self, url)
